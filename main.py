b8b70994730cd1f79a8799ef2e3ced51bfb27575
Trigun Joshi <iamtrigun@gmail.com>
2023-09-04 22:22:29 -0700

    initial setup

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("flash Card")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas()
+
+window.mainloop()
