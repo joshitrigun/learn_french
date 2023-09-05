a2444078428dbf15c409a44d2f83e57b93368a9f
Trigun Joshi <iamtrigun@gmail.com>
2023-09-04 23:43:02 -0700

    save progress

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
 current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
-# print(to_learn)
 
 
 def next_card():
@@ -28,6 +33,14 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card["English"])
     canvas.itemconfig(card_background, image=card_back_image)
 
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    # index=false because when saving we dont want index to be saved again and edit data structure
+    data.to_csv("data/words_to_learn.csv", index=False)
+
+    next_card()
 
 window = Tk()
 window.title("flash Card")
@@ -49,7 +62,7 @@ unknown_button = Button(image=cross_image, highlightthickness=0, command=next_ca
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image, highlightthickness=0, command=next_card)
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
