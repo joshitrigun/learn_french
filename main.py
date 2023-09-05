108f15235346457853cc3a5fda55ee5c5de1bb7f
Trigun Joshi <iamtrigun@gmail.com>
2023-09-04 23:20:30 -0700

    flip card functionaity added after wating 3 secs

--- main.py
+++ main.py
@@ -6,26 +6,38 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
+current_card = {}
 
 
 # print(to_learn)
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
     # print(current_card["French"])
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_image)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"])
+    canvas.itemconfig(card_background, image=card_back_image)
 
 
 window = Tk()
 window.title("flash Card")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
 canvas = Canvas(width=800, height=526)
-
 card_front_image = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_image)
+card_back_image = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_image)
 card_title = canvas.create_text(400, 163, text="", font=("Arial", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Arial", 58, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
